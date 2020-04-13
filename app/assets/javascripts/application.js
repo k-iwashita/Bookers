8d14a1f23ca5802fe57e632cd33231652e9c44b1
k-iwashita <k.iwashita.gude@gmail.com>
2020-04-13 23:16:32 +0900

    いいね非同期

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require rails-ujs
+
 //= require_tree .
 //= require bootstrap-sprockets
